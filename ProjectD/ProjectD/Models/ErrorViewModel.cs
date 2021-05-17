a0592736e67f100a8b89d3ea6d3585f2ab796833
Audrynass <55562216+AudriusPaulaitis@users.noreply.github.com>
2021-05-17 10:27:54 +0200

    ASP.NET Core project opgezet

---
+++ ProjectD/ProjectD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
