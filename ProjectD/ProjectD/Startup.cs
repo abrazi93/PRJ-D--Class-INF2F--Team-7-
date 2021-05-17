a72999a3b7a2d786b1746f6172c2ff55716cff6b
Audrynass <55562216+AudriusPaulaitis@users.noreply.github.com>
2021-05-17 11:55:42 +0200

    Revert "Revert "Entity Franework opgezet""
    This reverts commit f920bee786608fdaabf0277e2e33aca60734db80.

--- ProjectD/ProjectD/Startup.cs
+++ ProjectD/ProjectD/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,8 @@ namespace ProjectD
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<DataDbContext>(options => 
+                options.UseMySQL(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
